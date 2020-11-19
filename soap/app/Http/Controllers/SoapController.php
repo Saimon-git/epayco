2518a969953f86009a7641b8ecec82fdf7ec8517
Simon Montoya <saimon@MacBook-Air-de-Simon.local>
2020-11-19 17:06:46 -0500

    soap api

---
+++ soap/app/Http/Controllers/SoapController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Providers\MethodsProvider;
+use Illuminate\Http\Request;
+
+use WSDL\Annotation\BindingType;
+use WSDL\Annotation\SoapBinding;
+use WSDL\Builder\WSDLBuilder;
+use WSDL\WSDL;
+
+class SoapController extends Controller
+{
+    public function server()
+    {
+
+        $builder = WSDLBuilder::instance()
+            ->setName('payco')
+            ->setTargetNamespace('http://localhost/epayco/soap/public/api/server')
+            ->setNs('http://foo.bar/documentliteralservice/types')
+            ->setLocation('http://localhost/epayco/soap/public/api/server')
+            ->setPortName('payco')
+            ->setStyle(SoapBinding::DOCUMENT)
+            ->setUse(SoapBinding::LITERAL)
+            ->setSoapVersion(BindingType::SOAP_11)
+            ->setMethods(MethodsProvider::get());
+
+        $wsdl = WSDL::fromBuilder($builder);
+
+        if (isset($_GET['wsdl'])) {
+            header("Content-Type: text/xml");
+            echo $wsdl->create();
+            exit;
+        }
+        $server = new \SoapServer(NULL, [
+            'uri' => $builder->getTargetNamespace(),
+            'location' => $builder->getLocation(),
+            'style' => SOAP_DOCUMENT,
+            'use' => SOAP_LITERAL
+        ]);
+        $server->setClass('App\Http\Controllers\ServiceController');
+        $server->handle();
+        exit;
+    }
+}
